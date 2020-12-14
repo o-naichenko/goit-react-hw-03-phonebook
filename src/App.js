7af80e518c965d71dcea01209014ca58cc5cab2a
o-naichenko <o.naichenko@gmail.com>
2020-12-14 18:09:56 +0200

    Додає запис та зчитування контактів в/з localStorage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import s from './App.module.css';
-import ContactForm from './components/Contact-form';
-import Filter from './components/Filter';
-import ContactList from './components/Contact-list';
+import s from "./App.module.css";
+import ContactForm from "./components/Contact-form";
+import Filter from "./components/Filter";
+import ContactList from "./components/Contact-list";
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    contacts: [],
+    filter: "",
   };
   static propTypes = {
     value: PropTypes.string,
@@ -24,7 +19,7 @@ class App extends Component {
       id: PropTypes.string.isRequired,
     }),
   };
-  addContact = newContact => {
+  addContact = (newContact) => {
     if (this.checkContactUniqueness(newContact)) {
       this.setState(({ contacts }) => ({
         contacts: [newContact, ...contacts],
@@ -35,12 +30,21 @@ class App extends Component {
   };
   checkContactUniqueness(newContact) {
     return this.state.contacts.every(
-      contact => contact.name.toLowerCase() !== newContact.name.toLowerCase(),
+      (contact) => contact.name.toLowerCase() !== newContact.name.toLowerCase()
     );
   }
-  deleteContact = e => {
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+  componentDidUpdate() {
+    localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  }
+  deleteContact = (e) => {
     const filteredContacts = this.state.contacts.filter(
-      contact => contact.id !== e.currentTarget.id,
+      (contact) => contact.id !== e.currentTarget.id
     );
     this.setState({
       contacts: filteredContacts,
@@ -50,12 +54,12 @@ class App extends Component {
     if (this.state.filter.length === 0) {
       return this.state.contacts;
     } else {
-      return this.state.contacts.filter(contact =>
-        contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
+      return this.state.contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
       );
     }
   };
-  setFilterValue = value => this.setState({ filter: value });
+  setFilterValue = (value) => this.setState({ filter: value });
   render() {
     const { contacts, filter } = this.state;
     return (
